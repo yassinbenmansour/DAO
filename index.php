ff60ffd4f47627396f774a7ec5b5bf6c9619b845
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-09-01 21:29:05 +0100

    add function update

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
       require "./app/Models/Students.php";
 
       $Student = new Student();
+$Student->setNom("oussama");
+$Student->setPrenom("binsss");
+$Student->setAge(92);
+$Student->setEmail("bnissss@gmail.com");
+$Student->setPassword(798838897);
 
-      $Student->destroy(1);
+print_r($Student->Update(4));
